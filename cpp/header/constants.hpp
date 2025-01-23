72cfece349c649e70700f02df33f02296f00fed7
Leandro Zanotto <leandro.zanotto@gmail.com>
2025-01-23 17:20:45 -0300

    added constants

---
+++ cpp/header/constants.hpp
@@ -0,0 +1,24 @@
+/*
+ * This program is licensed granted by the University of Brasília - UnB (the “University”)
+ * for use of SIMULATION OF MAGNETIC SUSPENSIONS - SIMMSUS (“the Software”) through this website
+ * https://github.com/rafaelgabler/simmsus (the ”Website”).
+ *
+ * By downloading the Software through the Website, you (the “Licensee”) are confirming that you agree
+ * that your use of the Software is subject to the academic license terms.
+ *
+ * For more information about SIMMSUS please contact: rafael.gabler@unb.br (Rafael Gabler Gontijo)
+ * or leandro.zanotto@gmail.com (Leandro Zanotto).
+ *
+ */
+
+#ifndef SRC_HEADERS_CONSTANTS_HPP_
+#define SRC_HEADERS_CONSTANTS_HPP_
+
+#include <math.h>
+// Pre-calibrated numerical variables
+const int nb = 125; // number of phiscal cells for periodic interactions
+const int nbr = 27; // number of reciprocal cells for periodic interactions
+const double stokes = 1.0E-01; //! rotational Stokes number (Str)
+
+
+#endif /* SRC_HEADERS_CONSTANTS_HPP_ */
