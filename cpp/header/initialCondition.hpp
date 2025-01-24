64fdc22b5a2917a5727bbfbf2ec82f242d65bdea
Leandro Zanotto <leandro.zanotto@gmail.com>
2025-01-24 16:43:27 -0300

    New Header

---
+++ cpp/header/initialCondition.hpp
@@ -0,0 +1,20 @@
+/*
+ * This program is licensed granted by the University of Brasília - UnB (the “University”)
+ * for use of SIMULATION OF MAGNETIC SUSPENSIONS - SIMMSUS (“the Software”) through this website
+ * https://github.com/rafaelgabler/simmsus (the ”Website”).
+ *
+ * By downloading the Software through the Website, you (the “Licensee”) are confirming that you agree
+ * that your use of the Software is subject to the academic license terms.
+ *
+ * For more information about SIMMSUS please contact: rafael.gabler@unb.br (Rafael Gabler Gontijo)
+ * or leandro.zanotto@gmail.com (Leandro Zanotto).
+ *
+ */
+
+#ifndef SRC_HEADERS_INITIALCONDITION_HPP_
+#define SRC_HEADERS_INITIALCONDITION_HPP_
+
+void initialCondition(bool initialSphericalAggregate, bool ordenado, double *X0, double *X1, double *X2);
+
+
+#endif /* SRC_HEADERS_INITIALCONDITION_HPP_ */
